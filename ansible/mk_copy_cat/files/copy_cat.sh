43331e48404be055e0d1cc5f5a23ab2c1455ebab
Aleksandr Kravtsov <kr-al-ko@yandex.ru>
2025-03-10 20:28:58 +0300

    fix bag in copy_cat.sh

--- ansible/mk_copy_cat/files/copy_cat.sh
+++ ansible/mk_copy_cat/files/copy_cat.sh
@@ -2,7 +2,7 @@
 
 #Архивируем каталог и копируем на другую машину
 
-ARCHIV_NAME=mediawiki_`date +%Y%m%d_%H%M%S`
+ARCHIV_NAME=mediawiki_`date +%Y%m%d_%H%M%S`.tar.gz
 BACKUP_SERVER=192.168.10.25
 USG_MSG="Usage: copy_cat.sh path_to_target_catalog"
 ERR_MSG="Bad argument!"
@@ -22,18 +22,18 @@ fi
 PATH_T=`dirname $1`
 
 #Сформировать архив в каталоге цели
-tar -czf $PATH_T/$ARCHIV_NAME $1
+sudo tar -czf $PATH_T/$ARCHIV_NAME $1
 
 #Проверить доступность сервера для копирования
 ping -c 1 $BACKUP_SERVER > /dev/null
 
 if [ $? -eq 0 ]; then
     #Если сервер доступен - копируем созданный архив молча
-    scp $PATH_T/$ARCHIV_NAME madmin@$BACKUP_SERVER:/backup_cat
+    scp $PATH_T/$ARCHIV_NAME madmin@$BACKUP_SERVER:backup_cat
 else
     #Если не доступен - так и говорим
     echo "Server $BACKUP_SERVER is not available. Backup not created"
 fi
 
 #Удаляем локальный архив
-rm $PATH_T/$ARCHIV_NAME
+sudo rm $PATH_T/$ARCHIV_NAME
